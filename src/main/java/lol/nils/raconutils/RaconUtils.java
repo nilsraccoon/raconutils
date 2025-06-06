485be3a6a171b15752db1831fd1cf9212ad95c56
Nils <githubnils@gmail.com>
2025-06-06 16:43:23 +0200

    added module system, clickgui (p a i n) and autoblorp

--- src/main/java/lol/nils/raconutils/RaconUtils.java
+++ src/main/java/lol/nils/raconutils/RaconUtils.java
@@ -1,10 +1,16 @@
 package lol.nils.raconutils;
 
 import lol.nils.raconutils.event.AutoFriend;
+import lol.nils.raconutils.event.BlockManager;
+import lol.nils.raconutils.gui.KeybindHandler;
+import lol.nils.raconutils.gui.ModuleConfig;
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.common.config.Configuration;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -12,12 +18,35 @@ import org.apache.logging.log4j.Logger;
 public class RaconUtils
 {
     public static final String MODID = "raconutils";
-    public static final String VERSION = "1.0";
-    Logger logger = LogManager.getLogger(RaconUtils.MODID);
+    public static final String VERSION = "1.1";
+    public static final String prefix = EnumChatFormatting.GRAY + "[" + EnumChatFormatting.DARK_PURPLE + "rac" + EnumChatFormatting.LIGHT_PURPLE + "onut" + EnumChatFormatting.WHITE + "ils" + EnumChatFormatting.GRAY + "] ";
+    public static Logger logger = LogManager.getLogger(RaconUtils.MODID);
+    public static Configuration config;
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        config = new Configuration(event.getSuggestedConfigurationFile());
+        loadConfig();
+        ModuleConfig.initialize(config);
+    }
+
+    private void loadConfig() {
+        config.load();
+        
+        if (config.hasChanged()) {
+            config.save();
+        }
+    }
+
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
         MinecraftForge.EVENT_BUS.register(new AutoFriend());
-        logger.debug("Registered AutoFriend event listener");
+        AutoFriend.registerCommand();
+        BlockManager.initialize(config);
+        KeybindHandler.initialize();
+        MinecraftForge.EVENT_BUS.register(new KeybindHandler());
+        logger.debug("Registered AutoFriend event listener and command");
+        logger.debug("Registered BlockManager");
+        logger.debug("Registered KeybindHandler");
     }
 }
--- src/main/java/lol/nils/raconutils/event/AutoFriend.java
+++ src/main/java/lol/nils/raconutils/event/AutoFriend.java
@@ -1,30 +1,87 @@
 package lol.nils.raconutils.event;
 
+import lol.nils.raconutils.RaconUtils;
+import lol.nils.raconutils.gui.ModuleConfig;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.client.ClientCommandHandler;
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.EnumChatFormatting;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static lol.nils.raconutils.RaconUtils.prefix;
+
 public class AutoFriend {
+    public static boolean isEnabled() {
+        return enabled;
+    }
+
+    public static void setEnabled(boolean enabled) {
+        AutoFriend.enabled = enabled;
+        ModuleConfig.setState("AutoFriend", enabled);
+    }
+
     // §9§m-----------------------------------------------------§r§9
     //§r§eFriend request from §r§b[MVP§r§0+§r§b] AngelosGR§r§9
     //§r§a§l[ACCEPT]§r§8 - §r§c§l[DENY]§r§8 - §r§7§l[BLOCK]§r§9
     //§r§9§m-----------------------------------------------------§r
+    private static boolean enabled = true;
     private static final Pattern COLOR_CODE_PATTERN = Pattern.compile("§[0-9A-FK-ORa-fk-or]");
 
     public static String stripColorCodes(String input) {
         return COLOR_CODE_PATTERN.matcher(input).replaceAll("");
     }
 
+    public static void registerCommand() {
+        ClientCommandHandler.instance.registerCommand(new CommandBase() {
+            @Override
+            public String getCommandName() {
+                return "autofriend";
+            }
+
+            @Override
+            public String getCommandUsage(ICommandSender sender) {
+                return "/autofriend [toggle]";
+            }
+
+            @Override
+            public void processCommand(ICommandSender sender, String[] args) throws CommandException {
+                if (args.length > 0 && args[0].equalsIgnoreCase("toggle")) {
+                    setEnabled(!enabled);
+                    RaconUtils.config.get("autofriend", "enabled", true).set(enabled);
+                    RaconUtils.config.save();
+                    sender.addChatMessage(new ChatComponentText(prefix +
+                            EnumChatFormatting.WHITE + "AutoFriend is now " +
+                            (enabled ? EnumChatFormatting.GREEN + "enabled" : EnumChatFormatting.RED + "disabled")
+                    ));
+                } else {
+                    sender.addChatMessage(new ChatComponentText(prefix +
+                            EnumChatFormatting.WHITE + "AutoFriend is currently " +
+                            (enabled ? EnumChatFormatting.GREEN + "enabled" : EnumChatFormatting.RED + "disabled")
+                    ));
+                }
+            }
+
+            @Override
+            public int getRequiredPermissionLevel() {
+                return 0;
+            }
+        });
+    }
+
     @SubscribeEvent(priority = EventPriority.HIGH)
     public void onServerChat(ClientChatReceivedEvent event) {
+        if (!enabled) return;
         String cleanMessage = stripColorCodes(event.message.getFormattedText());
 
         if (cleanMessage.startsWith("-----------------------------------------------------") && cleanMessage.contains("Friend request from")) {
-            // todo config and toggle command
             Pattern pattern = Pattern.compile("Friend request from (?:\\[[^\\]]+\\] )*(\\w+)");
             Matcher matcher = pattern.matcher(cleanMessage);
 
@@ -34,5 +91,4 @@ public class AutoFriend {
             }
         }
     }
-
 }
